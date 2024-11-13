4822e8e65414079886fb74b16272b9e846f7467b
Tarikuzuma <edwinjrgumba@gmail.com>
2024-11-13 15:14:37 +0800

    feat: books display

---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="mb-10 text-2xl">Books</h1>
+
+    <form>
+
+    </form>
+
+    <ul>
+        @forelse($books as $book)
+        <li class="mb-4">
+            <div class="book-item">
+                <div class="flex flex-wrap items-center justify-between">
+                    <div class="w-full flex-grow sm:w-auto">
+                        <a href="{{ route('books.show', $book) }}" class="book-title">{{ $book->title }}</a>
+                        <span class="book-author">{{$book -> author}}</span>
+                    </div>
+                    <div>
+                        <div class="book-rating">
+                            {{number_format($book->reviews_avg_rating, 1)}}
+                        </div>
+                        <div class="book-review-count">
+                            out of {{$book->reviews_count}} review(s)
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </li>
+        @empty
+            <div class="empty-book-item">
+                <li class="empty-book-item">
+                    <p class="empty-text">No books found.</p>
+                    <a href="{{route('books.index')}}" class="reset-link">Reset Criteria</a>
+                </li>
+            </div>
+        @endforelse
+    </ul>
+@endsection
