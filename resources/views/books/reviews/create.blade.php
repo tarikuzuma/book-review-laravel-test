216d9b8acb12a40e646ee4831738e655ec9a1ad3
Tarikuzuma <edwinjrgumba@gmail.com>
2024-11-14 15:07:51 +0800

    feat: added a review feature finalized

--- resources/views/books/reviews/create.blade.php
+++ resources/views/books/reviews/create.blade.php
@@ -3,7 +3,7 @@
 @section ('content')
     <h1 class="mb-10 text-2xl">Add Review for {{ $book->title }}</h1>
 
-    <form method = "POST" action ="{{{route('books.reviews.store', $book)}}">
+    <form method = "POST" action ="{{route('books.reviews.store', $book)}}">
         @csrf
         <label for ="review"> Review </label>
         <textarea name="review" id="review" required class="input mb-4"></textarea>
